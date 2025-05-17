2350bdecee660dd3ced722141c39b7aa2b4ffb90
Matthew Law <matthew.law@standardbank.co.za>
2025-05-17 11:30:10 +0200

    Creating Demo class for condition expressions

--- Simple.Interpreter.Demo/Program.cs
+++ Simple.Interpreter.Demo/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Simple.Interpreter.Demo
+﻿using Simple.Interpreter.Ast;
+using Simple.Interpreter.Demo.Examples;
+using System;
+using System.Linq.Expressions;
+
+namespace Simple.Interpreter.Demo
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConditionExpressionDemo.Run();
         }
     }
 }
