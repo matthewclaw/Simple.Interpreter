5e18d34c68f6284b2c6af44a424ffb0a4d19fab5
Matthew Law <matthew.law@standardbank.co.za>
2025-05-19 17:44:08 +0200

    Adding Scope and Validation tests

--- Simple.Interpreter.Tests/Ast/Expression/TestCases/ExpressionTranslationTestCases.cs
+++ Simple.Interpreter.Tests/Ast/Expression/TestCases/ExpressionTranslationTestCases.cs
@@ -15,10 +15,15 @@ namespace Simple.Interpreter.Tests.Ast.Expression.TestCases
             yield return new ExpressionTranslationTest("3*2", "3 * 2");
             yield return new ExpressionTranslationTest("3.0*2.0", "3f * 2f");
             yield return new ExpressionTranslationTest("value in [4,2]", "(new int[] { 4, 2 }).Contains(value)");
+            yield return new ExpressionTranslationTest("value in [4.3,2.5]", "(new double[] { 4.3f, 2.5f }).Contains(value)");
+            yield return new ExpressionTranslationTest("value in ['foo', 'bar']", "(new string[] { \"foo\", \"bar\" }).Contains(value)");
             yield return new ExpressionTranslationTest("value not in [4,2]", "!(new int[] { 4, 2 }).Contains(value)");
             yield return new ExpressionTranslationTest("value and test", "(value) && (test)");
             yield return new ExpressionTranslationTest("value or test", "(value) || (test)");
             yield return new ExpressionTranslationTest("'42'", "\"42\"");
+            yield return new ExpressionTranslationTest("variable.MyProperty", "variable.MyProperty");
+            yield return new ExpressionTranslationTest("variable.MyMethod('foo')", "variable.MyMethod(\"foo\")");
+            yield return new ExpressionTranslationTest("'42' if(true) else '69'", "(true)? \"42\" : \"69\"");
         }
 
         IEnumerator IEnumerable.GetEnumerator()
