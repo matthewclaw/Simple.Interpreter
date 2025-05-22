23bb9a981e2f169bfbf540c9e9b166a48a33933f
MatthewLawCandB <52734992+MatthewLawCandB@users.noreply.github.com>
2025-05-22 22:11:32 +0200

    Adding test for exception

--- Simple.Interpreter.Tests/Extensions/CollectionExtensionTests.cs
+++ Simple.Interpreter.Tests/Extensions/CollectionExtensionTests.cs
@@ -9,49 +9,7 @@ namespace Simple.Interpreter.Tests.Extensions
 {
     public class CollectionExtensionTests
     {
-        [Fact]
-        public void Where_EmptySource_ReturnsEmptySequence()
-        {
-            // Arrange
-            var source = new List<int>();
-            string variableName = "x";
-            string expressionStr = "x > 2";
-
-            // Act
-            var result = source.Where(variableName, expressionStr);
-
-            // Assert
-            Assert.Empty(result);
-        }
-        [Fact]
-        public void Where_WithStartsWith_ReturnsFilteredSequence()
-        {
-            // Arrange
-            var source = new List<string> { "apple", "banana", "apricot" };
-            string variableName = "x";
-            string expressionStr = "startsWith(x,'a')";
-
-            // Act
-            var result = source.Where(variableName, expressionStr);
-
-            // Assert
-            Assert.Equal(new List<string> { "apple", "apricot" }, result.ToList());
-        }
-
-        [Fact]
-        public void Where_WithLengthGreaterThan5_ReturnsFilteredSequence()
-        {
-            // Arrange
-            var source = new List<string> { "foo","bar", "equivalent", "Hello Unit Test!", "xyz" };
-            string variableName = "x";
-            string expressionStr = "length(x) is greater than 5";
-
-            // Act
-            var result = source.Where(variableName, expressionStr);
-
-            // Assert
-            Assert.Equal(new List<string> { "equivalent", "Hello Unit Test!" }, result.ToList());
-        }
+        #region Public Methods
 
         [Fact]
         public void Select_WithSimpleExpression_ReturnsTransformedSequence()
@@ -67,5 +25,74 @@ namespace Simple.Interpreter.Tests.Extensions
             // Assert
             Assert.Equal(new List<string> { "1!", "2!", "3!" }, result.ToList());
         }
+
+        [Fact]
+        public void Where_EmptySource_ReturnsEmptySequence()
+        {
+            // Arrange
+            var source = new List<int>();
+            string variableName = "x";
+            string expressionStr = "x > 2";
+
+            // Act
+            var result = source.Where(variableName, expressionStr);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Where_WithLengthGreaterThan5_ReturnsFilteredSequence()
+        {
+            // Arrange
+            var source = new List<string> { "foo", "bar", "equivalent", "Hello Unit Test!", "xyz" };
+            string variableName = "x";
+            string expressionStr = "length(x) is greater than 5";
+
+            // Act
+            var result = source.Where(variableName, expressionStr);
+
+            // Assert
+            Assert.Equal(new List<string> { "equivalent", "Hello Unit Test!" }, result.ToList());
+        }
+
+        [Fact]
+        public void Where_WithNonBoolExpression_ThrowsException()
+        {
+            // Arrange
+            var source = new List<string> { "foo", "bar", "equivalent", "Hello Unit Test!", "xyz" };
+            string variableName = "x";
+            string expressionStr = "length(x)+4";
+            bool exceptionWasThrown = false;
+
+            // Act
+            try
+            {
+                _ = source.Where(variableName, expressionStr).ToArray();
+            }
+            catch (Exception ex)
+            {
+                exceptionWasThrown = true;
+            }
+            // Assert
+            Assert.True(exceptionWasThrown);
+        }
+
+        [Fact]
+        public void Where_WithStartsWith_ReturnsFilteredSequence()
+        {
+            // Arrange
+            var source = new List<string> { "apple", "banana", "apricot" };
+            string variableName = "x";
+            string expressionStr = "startsWith(x,'a')";
+
+            // Act
+            var result = source.Where(variableName, expressionStr);
+
+            // Assert
+            Assert.Equal(new List<string> { "apple", "apricot" }, result.ToList());
+        }
+
+        #endregion Public Methods
     }
 }
