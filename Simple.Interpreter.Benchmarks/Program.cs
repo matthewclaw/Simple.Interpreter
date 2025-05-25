ca6312ea0a26b500971abdb38511438efcc54ad4
Matthew Law <matthew.law@standardbank.co.za>
2025-05-25 11:38:19 +0200

    Adding init project

---
+++ Simple.Interpreter.Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple.Interpreter.Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
